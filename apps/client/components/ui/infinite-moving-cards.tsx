86d0140ce875bda93acc8aeebee28536fc3d063a
Abhishek Jha <iamabhishek1310@gmail.com>
2025-08-07 21:29:32 +0530

    Update infinite-moving-cards.tsx

--- apps/client/components/ui/infinite-moving-cards.tsx
+++ apps/client/components/ui/infinite-moving-cards.tsx
@@ -80,7 +80,7 @@ export const InfiniteMovingCards = ({
       <ul
         ref={scrollerRef}
         className={cn(
-          "flex w-max min-w-full shrink-0 flex-nowrap gap-4 py-4",
+          "flex justify-center w-full shrink-0 flex-nowrap gap-4 py-4",
           start && "animate-scroll",
           pauseOnHover && "hover:[animation-play-state:paused]"
         )}
